e83cd1e6996a0964e56bbc369c5700c9180824ba
praveenkumarguntoju <gpravin999@gmail.com>
2017-09-20 11:08:26 +0530

    new app

---
+++ app.js
@@ -0,0 +1,68 @@
+var app = require("express")();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var formidable = require('formidable');
+var fs = require('fs');
+
+app.get('/',function(req,res){
+    res.sendfile('index.html');
+});
+
+// app.post('/fileupload',function(req,res){
+//     var form = new formidable.IncomingForm();
+//      var oldpath = files.filetoupload.path;
+//       var newpath = 'C:/Users/' + files.filetoupload.name;
+//       fs.rename(oldpath, newpath, function (err) {
+//         if (err) throw err;
+//         res.write('File uploaded and moved!');
+//         res.end();
+//       });
+// });
+
+var clients = 0;
+
+io.on('connection',function(socket,formidable,fs){
+    console.log(fs);
+    socket.formidable = formidable;
+    socket.files = fs;
+    clients++;
+   console.log('A user connected');
+
+// broadcasting
+io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
+//end
+
+//clients disconnected
+ socket.on('disconnect', function () {
+	       clients--;
+	         io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
+ });
+//end
+
+
+setTimeout(function() {
+    socket.send("Message from a socket event");
+},4000);
+
+// creating a custom event
+setTimeout(function() {
+    socket.emit('testEvent',{description:"A custom event fired"});
+}, 2000);
+
+// end
+
+// custome event from client
+socket.on('clientEvent', function(data){
+    console.log(fs);
+     io.sockets.emit('passData',{description:data.sendData, userName : data.sendName, fileValue: data.FileData});
+ });
+//end
+
+socket.on('disconnect',function(){
+    console.log('User disconnected.');
+});
+});
+
+http.listen(8082,function(){
+    console.log("The server started.");
+})
